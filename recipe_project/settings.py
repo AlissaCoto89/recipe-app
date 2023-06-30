767bf9bcb3903d1b3f6fdecf7edbb80517301226
AlissaCoto89 <acoto189@gmail.com>
2023-06-30 18:04:04 -0400

    media files

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -113,10 +113,17 @@ STATICFILES_DIRS = [
 ]
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media' 
+
+
+
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
@@ -145,4 +152,4 @@ STORAGES = {
     },
 }
 
-WHITENOISE_ROOT = os.path.join(BASE_DIR, "media")
+# WHITENOISE_ROOT = os.path.join(BASE_DIR, "media")
