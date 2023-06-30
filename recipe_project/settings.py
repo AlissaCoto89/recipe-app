3a4bd421f8b427e48ecb2b6863e4a51f96483257
AlissaCoto89 <acoto189@gmail.com>
2023-06-30 16:52:12 -0400

    troubleshoot

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.core.files.storage.FileSystemStorage'
     # recipe project related apps,
     'recipes',
     'users',
